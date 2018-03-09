b55bf3b7f84947b02d8df002227a372abe8336d7
Caleb Lee <caleb.gwl@gmail.com>
2018-03-09 14:19:42 -0600

    2 Tabs

--- client/components/App.js
+++ client/components/App.js
@@ -4,14 +4,14 @@ import * as actionCreators from '../actions/actionCreator';
 import Main from './Main';
 
 function mapStateToProps(state) {
-    return {
-        posts: state.posts,
-        comments: state.comments
-    }
+  return {
+    posts: state.posts,
+    comments: state.comments
+  }
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators(actionCreators, dispatch);
+  return bindActionCreators(actionCreators, dispatch);
 }
 
 const App = connect(mapStateToProps, mapDispatchToProps)(Main);
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -5,11 +5,11 @@
 function posts(state=[], action) {
     switch(action.type) {
         case 'INCREMENT_LIKES':
-            const index = action.index;
+            const i = action.index;
             return [
-                ...state.slice(0, index), //before the one we are updating
-                {...state[index], likes: state[index].likes + 1},
-                ...state.slice(index + 1), // after the one we are updating
+                ...state.slice(0, i), //before the one we are updating
+                {...state[i], likes: state[i].likes + 1},
+                ...state.slice(i + 1), // after the one we are updating
             ]
         default:
             return state;
