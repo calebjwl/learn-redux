c545187fa6a81f9a6622ff3cf40f8b34f2fe003d
Caleb Lee <caleb.gwl@gmail.com>
2018-03-09 14:25:03 -0600

    Fix i/index issue

--- client/store.js
+++ client/store.js
@@ -8,8 +8,8 @@ import comments from './data/comments';
 import posts from './data/posts';
 
 const defaultState = {
-    posts,
-    comments
+  posts,
+  comments
 };
 
 const store = createStore(rootReducer, defaultState);
